069e4b7fcd4383689cca18163dbeb54c6c292950
masson <masson.teng@gmail.com>
2015-05-10 20:35:05 +0800

    fix unit test for executeCommandMarshalled()

--- command-router-libary/test/com/imasson/commandrouter/test/Address.java
+++ command-router-libary/test/com/imasson/commandrouter/test/Address.java
@@ -14,4 +14,26 @@ public class Address {
                 ", postcode='" + postcode + '\'' +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Address address = (Address) o;
+
+        if (city != null ? !city.equals(address.city) : address.city != null) return false;
+        if (postcode != null ? !postcode.equals(address.postcode) : address.postcode != null) return false;
+        if (street != null ? !street.equals(address.street) : address.street != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = street != null ? street.hashCode() : 0;
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (postcode != null ? postcode.hashCode() : 0);
+        return result;
+    }
 }
